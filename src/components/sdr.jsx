0d79df809edf5530a9a6c08ef6a74fde01a4df6a
ArturBlaya <you@example.com>
2022-07-29 18:05:44 +0200

    marges trets

--- src/components/sdr.jsx
+++ src/components/sdr.jsx
@@ -2,10 +2,10 @@ import styles from './sdr.module.css';
 
 export function Sdr() {
   return <div className={styles.container}>
-    <h2 className={styles.h2}>GENERATE SDR</h2>
-    
-    <h1 className={styles.h1}>Connect to a Report Suite</h1>
-    
+    <div className={styles.h_div}>
+      <h2 className={styles.h2}>GENERATE SDR</h2>
+      <h1 className={styles.h1}>Connect to a Report Suite</h1>
+    </div>
     <div className={styles.generate_report_div}>
       <select name="" id="">
         <option value="">Generate Suite 1</option>
