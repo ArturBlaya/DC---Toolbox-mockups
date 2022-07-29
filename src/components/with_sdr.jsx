c29ab72f73fc777b8ee33eb40f9174d47c27d1fd
ArturBlaya <you@example.com>
2022-07-29 21:46:09 +0200

    upgrades

--- src/components/with_sdr.jsx
+++ src/components/with_sdr.jsx
@@ -26,23 +26,47 @@ export function With_sdr() {
         <input type="text" name="" id="" value="Search props …"/>
         <button className={styles.button}>Push Changes</button>
       </div>
-      <table>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
-      </table>
+
+      <div className={styles.scroll_div}>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Allocation</th>
+            <th>Expiration</th>
+            <th>Status</th>
+            <th>Merchandising</th>
+            <th>binding Event</th>
+          </tr>
+          <tr>
+            <td>eVar Name</td>
+            <td>text</td>
+            <td>last</td>
+            <td>vist</td>
+            <td>subrelations</td>
+            <td>no</td>
+            <td>event</td>
+          </tr>
+          <tr>
+            <td>eVar Name</td>
+            <td>text</td>
+            <td>last</td>
+            <td>vist</td>
+            <td>subrelations</td>
+            <td>no</td>
+            <td>event</td>
+          </tr>
+          <tr>
+            <td>eVar Name</td>
+            <td>text</td>
+            <td>last</td>
+            <td>vist</td>
+            <td>subrelations</td>
+            <td>no</td>
+            <td>event</td>
+          </tr>
+        </table>
+      </div>
     </div>
 
   </div>
