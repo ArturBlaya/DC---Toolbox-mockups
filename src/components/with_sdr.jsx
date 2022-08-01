5df785acd8054229f296818f36be72c3231f05a3
ArturBlaya <you@example.com>
2022-08-01 11:52:58 +0200

    button state control

--- src/components/with_sdr.jsx
+++ src/components/with_sdr.jsx
@@ -5,17 +5,27 @@ import Props from './props';
 import Events from './events';
 
 
+
 export function With_sdr() {
   const [state, setState] = useState("");
   
   function updateState(text) {
     if (text==="eVars") {
+      document.getElementById("eVars").className = styles.button_clicked;
+      document.getElementById("props").className = styles.button;
+      document.getElementById("events").className = styles.button;
       setState(<EVars/>)
     }
     else if (text==="props") {
+      document.getElementById("eVars").className = styles.button;
+      document.getElementById("props").className = styles.button_clicked;
+      document.getElementById("events").className = styles.button;
       setState(<Props/>) 
     }
     else if (text==="events") {
+      document.getElementById("eVars").className = styles.button;
+      document.getElementById("props").className = styles.button;
+      document.getElementById("events").className = styles.button_clicked;
       setState(<Events/>)
     }
     
@@ -38,9 +48,9 @@ export function With_sdr() {
 
     <div className={styles.table_div}>
       <div className={styles.table_buttons_div}>
-        <button className={styles.button} onClick={() => updateState("eVars")}   >eVars</button>
-        <button className={styles.button} onClick={() => updateState("props")}   >props</button>
-        <button className={styles.button} onClick={() => updateState("events")}  >events</button>
+        <button id="eVars" className={styles.button} onClick={() => updateState("eVars")}   >eVars</button>
+        <button id="props" className={styles.button} onClick={() => updateState("props")}   >props</button>
+        <button id="events" className={styles.button} onClick={() => updateState("events")}  >events</button>
       </div>
       <div className={styles.search_div}>
         <input type="text" name="" id="" value="Search props …"/>
